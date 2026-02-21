3d0c13c6b9802a4af96a94a80e771cc6193d3e6e
dcdoublecool <74202400+dcdoublecool@users.noreply.github.com>
2026-02-22 02:33:52 +0800

    Update cookie and Telegram bot token in config

--- src/smzdm_bot/config/__init__.py
+++ src/smzdm_bot/config/__init__.py
@@ -113,7 +113,7 @@ class Settings(BaseSettings):
     """
 
     # Single user mode
-    cookie: str = ""
+    cookie: str = "r_sort_type=score; __ckguid=I9XyO3YUubPIvb2KQopSlx7; device_id=2130706433175102503834316721deffa8ad4a410a3c30e15d22f059da; homepage_sug=b; smzdm_user_source=3114A7551FFED5B39ED9F8082BDC30B4; footer_floating_layer=0; ss_ab=ss48; ssmx_ab=mxss70; s_his=M1%20Max%20Macbook%20Pro%2Cmac; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%222372836556%22%2C%22first_id%22%3A%221876fe688b1def-077e61a815d6a1-26031851-3686400-1876fe688b2f96%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%2C%22%24latest_landing_page%22%3A%22https%3A%2F%2Fsearch.smzdm.com%2F%3Fc%3Dhome%26s%3Dmac%26v%3Db%26mx_v%3Db%22%7D%2C%22%24device_id%22%3A%221876fe688b1def-077e61a815d6a1-26031851-3686400-1876fe688b2f96%22%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMTkxMTJmNzI5ZDgxNjVlLTAwZWQ1NWU1MmJjMTVhMS0yNjAwMWU1MS0zNjg2NDAwLTE5MTEyZjcyOWQ5ZjU4IiwiJGlkZW50aXR5X2xvZ2luX2lkIjoiMjM3MjgzNjU1NiJ9%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%24identity_login_id%22%2C%22value%22%3A%222372836556%22%7D%7D; Hm_lvt_9b7ac3d38f30fe89ff0b8a0546904e58=1771431621,1771578480,1771697921; HMACCOUNT=9D50F3DBC7D2229D; ad_date=22; bannerCounter=%5B%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%5D; ad_json_feed=%7B%7D; sess=BA-gKnSD36MFBag0QLH02e0H5SBcZ3sVRih922X5md3SFgpgGay3vxjOIXcEKR7TOlDvXtoG%2Fx71EV81RqI9fOobzjWNbAbuRdNUNIQvx3VXLq3nfRpjwIk4ok9; user=user%3A2372836556%7C2372836556; smzdm_id=2372836556; Hm_lpvt_9b7ac3d38f30fe89ff0b8a0546904e58=1771698123; w_tsfp=ltvuV0MF2utBvS0Q7qvvnUOvEzkhcDA4h0wpEaR0f5thQLErU5mC0IBztsjxOH3e5sxnvd7DsZoyJTLYCJI3dwNFFsqWINsShAnFkYFwj91GAEZgGcrdCARLcu4k7TlCdXhCNxS00jA8eIUd379yilkMsyN1zap3TO14fstJ019E6KDQmI5uDW3HlFWQRzaLbjcMcuqPr6g18L5a5Tjc5VL/JV9yBOxLgk3D134bCi0i4RC5fbxePB2ld8+mSqA="
     sk: str = ""
 
     # Multi-user mode (JSON array)
@@ -123,7 +123,7 @@ class Settings(BaseSettings):
     push_plus_token: str = ""
     sc_key: str = ""
     wecom_webhook: str = ""
-    tg_bot_token: str = ""
+    tg_bot_token: str = "8073394698:AAElVxdbqRzHFKfzdOmcAfH9EgGE1jcceI0"
     tg_user_id: str = ""
     tg_api_base: str = ""
 
